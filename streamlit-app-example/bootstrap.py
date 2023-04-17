3c4cb4203ecfc8eed507f7ce68161cc6e6a1f844
jychuah <jychuah@gmail.com>
2023-04-17 11:58:07 -0500

    Streamlit example (#24)
    * streamlit hello world
    
    * Provisional oauth flow
    
    * restore hub ip discovery
    
    * Auto redirect
    
    * redirect and hide sidebar
    
    * cleanup
    
    * Comments
    
    * Minor example tweaks
    
    * bump
    
    * Fix watch command
    
    * Readme update
    
    ---------
    
    Co-authored-by: Joon-Yee Chuah <jychuah@enthought.com>
    Co-authored-by: Andrew Collette <acollette@enthought.com>

---
+++ streamlit-app-example/bootstrap.py
@@ -0,0 +1,50 @@
+# Enthought product code
+#
+# (C) Copyright 2010-2022 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This file and its contents are confidential information and NOT open source.
+# Distribution is prohibited.
+
+"""
+    Bootstrap file for the dashboard example.
+"""
+
+import subprocess
+
+ENV_NAME = "edge-streamlit-dev"
+
+EDM_DEPS = [
+    "click",
+    "jupyterhub",
+]
+
+PIP_DEPS = [
+    "sqlalchemy<2",
+    "dockerspawner",
+    "streamlit",
+    "streamlit-javascript",
+    "streamlit-extras"
+]
+
+def bootstrap():
+    """Create and populate dev env"""
+
+    print("Creating EDM Python environment...")
+    cmd = ["edm", "envs", "create", ENV_NAME, "--version", "3.8", "--force"]
+    subprocess.run(cmd, check=True)
+
+    print("Installing EDM dependencies...")
+    cmd = ["edm", "install", "-e", ENV_NAME, "-y"] + EDM_DEPS
+    subprocess.run(cmd, check=True)
+
+    print("Installing pip dependencies...")
+    cmd = ["edm", "run", "-e", ENV_NAME, "--", "pip", "install"] + PIP_DEPS
+    subprocess.run(cmd, check=True)
+
+    print("Bootstrap complete.")
+    print(f'To use your new dev environment, run "edm shell -e {ENV_NAME}"')
+
+
+if __name__ == "__main__":
+    bootstrap()
---
+++ streamlit-app-example/ci/__main__.py
@@ -0,0 +1,99 @@
+# Enthought product code
+#
+# (C) Copyright 2010-2022 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This file and its contents are confidential information and NOT open source.
+# Distribution is prohibited.
+
+import os
+import subprocess
+
+import click
+
+STREAMLIT_EXAMPLE_IMAGE = "quay.io/enthought/edge-streamlit-demo"
+STREAMLIT_EXAMPLE_CONTAINER = "edge-streamlit-demo"
+MODULE_DIR = os.path.join(os.path.dirname(__file__), "..")
+SRC_DIR = os.path.join(MODULE_DIR, "src")
+
+
+@click.group()
+def cli():
+    """All commands constituting continuous integration."""
+    pass
+
+
+@cli.command("build")
+@click.option("--tag", default="latest", help="Docker tag to use.")
+def build(tag):
+    """Build the streamlit example app"""
+    click.echo("Building the streamlit Example App...")
+
+    cmd = [
+        "docker",
+        "build",
+        "-t",
+        f"{STREAMLIT_EXAMPLE_IMAGE}:{tag}",
+        "--build-arg",
+        f"CI_IMAGE_REPOSITORY={STREAMLIT_EXAMPLE_IMAGE}",
+        "--build-arg",
+        f"CI_IMAGE_TAG={tag}",
+        "-f",
+        "Dockerfile",
+        MODULE_DIR,
+    ]
+    subprocess.run(cmd, check=True)
+    click.echo("Done")
+
+
+@cli.command("publish")
+@click.option("--tag", default="latest", help="Docker tag to use.")
+def publish(tag):
+    """Publish the streamlit example app"""
+    click.echo("Publishing the streamlit Example App...")
+    cmd = ["docker", "push", f"{STREAMLIT_EXAMPLE_IMAGE}:{tag}"]
+    subprocess.run(cmd, check=True)
+    click.echo("Done")
+
+
+@cli.command("publish-hack")
+@click.option("--tag", default="latest", help="Docker tag to use.")
+def publish(tag):
+    """Publish to edge-native-app-flask-demo ("real" repo not ready)"""
+    click.echo("Publishing the streamlit Example App to old repo...")
+    xtag = "quay.io/enthought/edge-native-app-flask-demo:streamlit"
+    subprocess.run(
+        ["docker", "image", "tag", f"{STREAMLIT_EXAMPLE_IMAGE}:{tag}", xtag], check=True
+    )
+    cmd = ["docker", "push", xtag]
+    subprocess.run(cmd, check=True)
+    click.echo("Done")
+
+
+@cli.command("start")
+@click.option("--tag", default="latest", help="Docker tag to use.")
+def start(tag):
+    """Start the streamlit example application"""
+    click.echo("Starting the JupyterHub container...")
+    cmd = ["jupyterhub", "-f", "ci/jupyterhub_config.py"]
+    env = os.environ.copy()
+    env["IMAGE_TAG"] = tag
+    subprocess.run(cmd, check=True, env=env)
+    click.echo("JupyterHub is running at: http://127.0.0.1:8888")
+
+
+@cli.command("watch")
+def watch_cmd():
+    """Start the application and watch backend changes"""
+
+    print(f"\nStart {STREAMLIT_EXAMPLE_CONTAINER} in files watching mode\n")
+    cmd = ["streamlit", "run", "app.py"]
+    env = os.environ.copy()
+    env["STREAMLIT_DEBUG"] = "1"
+    env["JUPYTERHUB_API_TOKEN"] = "1"
+    env["APP_VERSION"] = "streamlit-app-example running on ci watch backend"
+    subprocess.run(cmd, check=True, env=env, cwd=SRC_DIR)
+
+
+if __name__ == "__main__":
+    cli(prog_name="python -m ci")
