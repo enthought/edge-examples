6a4461e26c3a39b28b164152615740f8bcedccca
Joon-Yee Chuah <jychuah@enthought.com>
2023-05-02 17:28:08 -0500

    nginx in supervisord

---
+++ .dev/edge-native-base/container_config/supervisor/nginx-startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+if [ -z $JUPYTERHUB_SERVICE_PREFIX]; then
+    export JUPYTERHUB_SERVICE_PREFIX='/';
+fi
+envsubst '${JUPYTERHUB_SERVICE_PREFIX}' < /opt/nginx/nginx.conf.template > /opt/nginx/nginx.conf;
+envsubst '${JUPYTERHUB_SERVICE_PREFIX}' < /opt/nginx/app.location.conf.template > /opt/nginx/app.location.conf;
+nginx -c /opt/nginx/nginx.conf;
