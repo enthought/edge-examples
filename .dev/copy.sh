9f0e4d1a88699a32eeba7a4849ee8be48968e765
Andrew Collette <acollette@enthought.com>
2023-05-15 14:47:41 -0500

    py->sh

---
+++ .dev/copy.sh
@@ -0,0 +1,5 @@
+cp -r ci-native-base/* ../example-dashboard
+cp -r ci-native-base/* ../example-flask
+cp -r ci-native-base/* ../example-panel
+cp -r ci-native-base/* ../example-plotly-dash
+cp -r ci-native-base/* ../example-streamlit
