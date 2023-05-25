7584154b8d0d35d319f18b8a9ae90002d167b7eb
Andrew Collette <acollette@enthought.com>
2023-05-25 11:28:17 -0500

    checkpoint

--- example-minimal/bootstrap.py
+++ example-minimal/bootstrap.py
@@ -15,25 +15,23 @@ import getopt
 import subprocess
 import sys
 
-from config import EDM_DEPS, ENV_NAME, PIP_DEPS
-
 ENV_NAME = "edge-minimal-example"
-EDM_DEPS = ["click"]
+EDM_DEPS = ["click", "pip", "setuptools"]
 PIP_DEPS = ["jupyterhub==2.2.2", "sqlalchemy<2", "dockerspawner"]
 
 
 def bootstrap():
     """Create and populate dev env"""
 
-    if ENV_NAME not in _list_edm_envs(base_cmd):
+    if ENV_NAME not in _list_edm_envs():
         print(f"Creating development environment {ENV_NAME}...")
         cmd = ["edm", "envs", "create", ENV_NAME, "--version", "3.8", "--force"]
         subprocess.run(cmd, check=True)
 
-        cmd = base_cmd + ["install", "-e", ENV_NAME, "-y"] + EDM_DEPS
+        cmd = ["edm", "install", "-e", ENV_NAME, "-y"] + EDM_DEPS
         subprocess.run(cmd, check=True)
 
-        cmd = base_cmd + ["run", "-e", ENV_NAME, "--", "pip", "install"] + PIP_DEPS
+        cmd = ["edm", "run", "-e", ENV_NAME, "--", "pip", "install"] + PIP_DEPS
         subprocess.run(cmd, check=True)
 
         print("Bootstrap complete.")
@@ -45,8 +43,8 @@ def bootstrap():
     subprocess.run(["edm", "shell", "-e", ENV_NAME])
 
 
-def _list_edm_envs(edm_base_cmd):
-    cmd = edm_base_cmd + ["envs", "list"]
+def _list_edm_envs():
+    cmd = ["edm", "envs", "list"]
     proc = subprocess.run(
         cmd, check=True, capture_output=True, encoding="utf-8", errors="ignore"
     )
