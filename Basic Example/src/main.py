11938b5d5175d2c983e2dfe72fd95af4e5708926
Andrew Collette <acollette@enthought.com>
2023-05-25 16:33:57 -0500

    checkpoint

---
+++ Basic Example/src/main.py
@@ -0,0 +1,89 @@
+# Enthought product code
+#
+# (C) Copyright 2010-2022 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This file and its contents are confidential information and NOT open source.
+# Distribution is prohibited.
+
+"""
+    Example Flask application (minimal).
+"""
+
+import os
+from flask import Flask, render_template_string
+
+from edge.api import EdgeSession
+
+
+def get_edge_session():
+    """Helper function to get an EdgeSession object.
+
+    EdgeSession will auto-load environment variables, including the API token,
+    location of the Edge server, etc. Please note:
+
+    1. In production, they are set by Edge itself; you don't have to do anything.
+    2. In testing, you can set values in "dev_settings.json"
+
+    Returns an EdgeSession object if one can be constructed, or None if the
+    required information is missing.
+    """
+
+    def is_set(name):
+        return name in os.environ
+
+    if (
+        is_set("EDGE_API_SERVICE_URL")
+        and is_set("EDGE_API_ORG")
+        and (is_set("EDGE_API_TOKEN") or is_set("JUPYTERHUB_API_TOKEN"))
+    ):
+        return EdgeSession()
+
+    return None
+
+
+edge_session = get_edge_session()
+app = Flask(__name__)
+
+
+@app.get("/")
+def root():
+    """Example Flask route"""
+
+    if edge_session is None:
+        html = """\
+        <html><body>
+        <h1>Hello World!</h1>
+        <p>To see more information, run this app in Edge or set up
+        the "dev_settings.json" file locally.</p>
+        </body></html>"""
+        return render_template_string(html)
+
+    html = """\
+    <html><body>
+    <h1>Hello {{ edge_session.whoami().user_name }}!</h1>
+    <p>
+    Welcome!  You are in the "{{ edge_session.organization }}" organization.
+    </p>
+    <p>
+    Here is a list of the files in your organization (top folder only):
+    <ul>
+    {% for fname in edge_session.files.list_files() %}
+    <li>{{ fname }}</li>
+    {% endfor %}
+    </ul>
+    </p>
+    <p>This example is served from: "{{ example_served_from }}".</p>
+    </body></html>
+    """
+
+    # See the README.md file for more info on how routes work.  You typically
+    # don't need to use this value in your code; your Flask routes, etc.,
+    # should be set up as though the app was located at the server root.
+    example_served_from = os.environ.get("JUPYTERHUB_SERVICE_PREFIX", "/")
+
+    return render_template_string(
+        html,
+        edge_session=edge_session,
+        example_served_from=example_served_from,
+    )
--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-## Edge examples gallery
+# Edge examples gallery
+
+![Screenshot of the Flask/React example](.dev/images/example-flask.png)
 
 This GitHub repo holds working examples of apps which run on Enthought Edge.
+
+Each example is self-contained, with its own README.md and "ci" module that
+contains build commands.  For new users, the recommended workflow for
+development is:
+
+* Make a copy of the example files.
+* Following the instructions in the README file, get to the point where the
+  example app is working locally.
+* Publish the example (possibly with minor changes to the config, such as adding
+  an image name that matches your Docker repository).
+* Ensure you are able to launch the app in Edge.
+* Modify your copy of the example as desired.
+
+
+## Basic Example
+
+It's recommended you start here.  The Basic Example is a simple Flask
+"Hello World" app, which shows how to build your application's Docker image,
+run and debug it locally, and finally publish it to Edge.  
+
+Using the Edge API (EdgeSession) within the app is also demonstrated.  This
+allows you to access files and data connectors stored in Edge, directly from
+your application code.
+
+
+## Advanced Examples
+
+These are somewhat more complicated, because each demonstrates the use of
+a particular feature or framework, for example Panel or Streamlit.  See the
+README.md for each example for details.
+
+
+
