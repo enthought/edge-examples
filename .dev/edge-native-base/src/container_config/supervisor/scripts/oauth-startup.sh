7f6cb18188f2e336b8d1839d7ad7ab4eeefe99a3
Andrew Collette <acollette@enthought.com>
2023-05-26 13:41:26 -0500

    Remove files exported to main

--- .dev/edge-native-base/src/container_config/supervisor/scripts/oauth-startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-exec 2>&1
-set -e
-export LC_ALL=en_US.utf-8
-export LANG=en_US.utf-8
-export SESSION_SECRET_KEY=`head -c 100 /dev/urandom | md5sum`
-cd /opt
-echo "Starting OAuth2 Proxy"
-edm -r /opt/_edm_oauth2_proxy run -- python -m oauth2_proxy.wsgi
