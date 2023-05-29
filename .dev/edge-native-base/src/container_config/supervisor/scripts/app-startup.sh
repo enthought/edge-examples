d4c014d4f4293bf9ebd74df148a1954981237507
Andrew Collette <acollette@enthought.com>
2023-05-29 14:22:32 -0500

    Remove files exported to main (#39)

--- .dev/edge-native-base/src/container_config/supervisor/scripts/app-startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Main application entrypoint
-
-exec 2>&1
-cd /home/app
-echo "Starting app"
-./startup-script.sh
--- .dev/edge-native-base/src/startup-script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/app/default
-exec edm -r /opt/_edm_oauth2_proxy run -- python -m http.server 9000
