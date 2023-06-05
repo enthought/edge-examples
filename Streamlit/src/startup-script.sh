99675f642bf2d3f7a980095093838998f4de2258
Andrew Collette <acollette@enthought.com>
2023-06-05 11:50:51 -0500

    Update Streamlit settings

--- Streamlit/src/startup-script.sh
+++ Streamlit/src/startup-script.sh
@@ -3,8 +3,8 @@
 set -e
 
 # This file will be automatically picked up by the edge-native-base machinery,
-# which runs a small proxy server in front of your app.  Your app must bind to
-# 127.0.0.1 on port 9000 for this to work.  The container itself will
-# serve on port 8888, through the proxy.
+# which runs a small proxy server in front of your app.  Your app must bind
+# to port 9000 for this to work.  The container itself will serve on port
+# 8888, through the proxy.
 
-exec edm run -- streamlit run app.py --server.headless true --server.address=127.0.0.1 --server.port 9000
+exec edm run -- streamlit run app.py --server.headless true --server.port 9000
