abd2838f2f488445890cb4fb07f030fda763b5ff
EdwinWiseOne <edwin@simreal.com>
2024-06-14 11:30:41 -0500

    Exploring the cli_support.py

---
+++ Minimal/cli_support.py
@@ -0,0 +1,12 @@
+# Enthought product code
+#
+# (C) Copyright 2010-2022 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This file and its contents are confidential information and NOT open source.
+# Distribution is prohibited.
+"""Things needed by the Edge CLI"""
+
+from bootstrap import ENV_NAME, EDM_DEPS
+
+TEST = "Fred Test"
